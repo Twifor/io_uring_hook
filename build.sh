73e51bad3af8a1ba9097660295341f64dbccc03c
Twiforest <liyuhan0@tju.edu.cn>
2024-08-16 15:55:06 +0800

    refine build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-cmake . -B build -DCMAKE_BUILD_TYPE=Release -DWITH_GLOG=ON
-cmake --build build -j8
+cmake . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DWITH_GLOG=ON
+cmake --build build -j8 --target test uringhook
 mv build/test ./
